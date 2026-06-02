75ad550eae4ca66c7de9f287df4a330ff7f62d70
0ZMilad <milad.amini2026@gmail.com>
2026-06-02 15:05:52 +0100

    fix(auth): expose auth loading state

--- apps/frontend/src/components/auth-provider.tsx
+++ apps/frontend/src/components/auth-provider.tsx
@@ -14,19 +14,21 @@ import { authService } from "@/middle-service/supabase";
 interface AuthContextType {
   session: Session | null;
   user: User | null;
+  isLoading: boolean;
 }
 
 const AuthContext = createContext<AuthContextType | null>(null);
 
 export const AuthProvider = ({ children }: PropsWithChildren) => {
   const [currentSession, setSession] = useState<Session | null>(null);
-
   const [currentUser, setUser] = useState<User | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     authService.getSession().then(({ data: { session } }) => {
       setSession(session);
       setUser(session?.user || null);
+      setIsLoading(false);
     });
   }, []);
 
@@ -41,8 +43,8 @@ export const AuthProvider = ({ children }: PropsWithChildren) => {
   }, []);
 
   const value = useMemo(
-    () => ({ session: currentSession, user: currentUser }),
-    [currentSession, currentUser]
+    () => ({ session: currentSession, user: currentUser, isLoading }),
+    [currentSession, currentUser, isLoading],
   );
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
